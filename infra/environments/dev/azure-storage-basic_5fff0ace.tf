2770f514d537a02bf8e73f2e12dc2b0d24145996
Christopher House <christopher.house@ipaper.com>
2025-11-16 13:54:55 -0600

    chore: remove unused Azure Key Vault and Storage configurations

--- infra/environments/dev/azure-storage-basic_5fff0ace.tf
+++
@@ -1,9 +0,0 @@
-module "azure-storage-basic_5fff0ace" {
-  source       = "../../modules/azure-storage-basic"
-  project_name = "asa-test2"
-  environment = "dev"
-  resource_group_name = "test"
-  location = "eastus2"
-  account_tier = "Standard"
-  replication_type = "LRS"
-}
