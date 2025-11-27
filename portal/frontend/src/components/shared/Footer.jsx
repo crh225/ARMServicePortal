6fe608ce96c778cbb9c80a36b888017e408ac67a
Chris House <926068@gmail.com>
2025-11-26 21:23:55 -0600

    change to kibana url

--- portal/frontend/src/components/shared/Footer.jsx
+++ portal/frontend/src/components/shared/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
       <span className="footer-links">
         <a href="https://argocd.chrishouse.io" target="_blank" rel="noopener noreferrer">ArgoCD</a>
         {" | "}
-        <a href="https://es-test-az-elk-managed-dev-5a6e80.es.eastus2.azure.elastic-cloud.com:9243" target="_blank" rel="noopener noreferrer">Elastic</a>
+        <a href="https://es-test-az-elk-managed-dev-5a6e80.kb.eastus2.azure.elastic-cloud.com:9243" target="_blank" rel="noopener noreferrer">Kibana</a>
       </span>
     </footer>
   );
