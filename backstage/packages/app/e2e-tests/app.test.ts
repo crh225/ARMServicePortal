eaa7aef4d497ff7628cb9a7839b150392704e784
Christopher House <christopher.house@ipaper.com>
2025-12-04 20:06:39 -0600

    fix urls

--- backstage/packages/app/e2e-tests/app.test.ts
+++ backstage/packages/app/e2e-tests/app.test.ts
@@ -23,5 +23,5 @@ test('App should render the welcome page', async ({ page }) => {
   await expect(enterButton).toBeVisible();
   await enterButton.click();
 
-  await expect(page.getByText('Chris House IDP Catalog')).toBeVisible();
+  await expect(page.getByText('Chrishouse.io Catalog')).toBeVisible();
 });
