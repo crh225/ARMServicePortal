0fe7522147edd159ddac99077cd1300065f26662
Christopher House <christopher.house@ipaper.com>
2025-12-04 20:49:46 -0600

    default kind dropdown

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -70,7 +70,7 @@ const app = createApp({
 const routes = (
   <FlatRoutes>
     <Route path="/" element={<Navigate to="catalog" />} />
-    <Route path="/catalog" element={<CatalogIndexPage />} />
+    <Route path="/catalog" element={<CatalogIndexPage initialKind="resource" />} />
     <Route
       path="/catalog/:namespace/:kind/:name"
       element={<CatalogEntityPage />}
