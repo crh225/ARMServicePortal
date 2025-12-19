d3849d6d7c3d02154d8ec5a31e7ac63ff810a337
Chris House <926068@gmail.com>
2025-12-18 22:14:38 -0600

    delete

--- infra/environments/dev/azure-rg-basic_6a67d2bb.tf
+++
@@ -1,7 +0,0 @@
-module "azure-rg-basic_6a67d2bb" {
-  source       = "../../modules/azure-rg-basic"
-  project_name = "rg-testpr2"
-  environment  = "dev"
-  location     = "eastus2"
-  request_id   = "3"
-}
