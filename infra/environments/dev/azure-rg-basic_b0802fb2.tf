9a6067a43ebaf247ddbb800e29d839822c83a322
Christopher House <christopher.house@ipaper.com>
2025-12-19 09:09:38 -0600

    remove

--- infra/environments/dev/azure-rg-basic_b0802fb2.tf
+++
@@ -1,7 +0,0 @@
-module "azure-rg-basic_b0802fb2" {
-  source       = "../../modules/azure-rg-basic"
-  project_name = "rg-testpr3"
-  environment  = "dev"
-  location     = "eastus2"
-  request_id   = "4"
-}
