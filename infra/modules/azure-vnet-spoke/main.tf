85a45b0a3e48ed2a6a37419919921fa950b7cc75
Chris House <926068@gmail.com>
2025-12-21 10:27:22 -0600

    HTTP (80) and HTTPS (443) rules to the AKS nodes subnet NSG.

--- infra/modules/azure-vnet-spoke/main.tf
+++ infra/modules/azure-vnet-spoke/main.tf
@@ -62,10 +62,36 @@ resource "azurerm_network_security_group" "aks_nodes" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  # Allow inbound from hub
+  # Allow HTTP inbound for external ingress
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTPS inbound for external ingress
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  # Allow inbound from hub/VNet
   security_rule {
     name                       = "AllowHubInbound"
-    priority                   = 100
+    priority                   = 120
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "*"
