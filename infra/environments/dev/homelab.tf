1314770e72f0eab3320f65308674d5e9e8f88f6e
Christopher House <christopher.house@ipaper.com>
2026-01-09 10:12:11 -0600

    remove script

--- infra/environments/dev/homelab.tf
+++ infra/environments/dev/homelab.tf
@@ -11,11 +11,3 @@ resource "azurerm_resource_group" "homelab" {
     "armportal-environment" = "shared"
   }
 }
-
-# Note: The raspberry-pi-5 Azure Arc machine will need to be moved manually:
-# az resource move \
-#   --destination-group rg-homelab \
-#   --ids "/subscriptions/f989de0f-8697-4a05-8c34-b82c941767c0/resourceGroups/rg-armportal-aks-crossplane-dev/providers/Microsoft.HybridCompute/machines/raspberry-pi-5"
-#
-# Then import into Terraform state if desired:
-# terraform import azurerm_arc_machine.raspberry_pi /subscriptions/.../providers/Microsoft.HybridCompute/machines/raspberry-pi-5
